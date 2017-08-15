b71ee7f5902407632f9e555aed161cc1b43f5831
Pavel <akulov_pavel88@mail.ru>
2017-08-15 18:54:38 +0300

    ++

--- server/exo.js
+++ server/exo.js
@@ -2,18 +2,30 @@ var http = require('http');
 var server = new http.Server();
 var url=require('url');
 
-server.on('request', function (req,res) {
-    console.log(req.method, req.url );
-    var urlParsed = url.parse(req.url, true);
+// server.on('request', function (req,res) {
+//     console.log(req.method, req.url );
+//     var urlParsed = url.parse(req.url, true);
+//     console.log(req.headers);
+//
+//     if(urlParsed.pathname=='/echo' && urlParsed.query.message){
+//         res.end(`it is ok. Message is ${urlParsed.query.message}`);
+//     }else{
+//         res.statusCode = 404;
+//         res.end("Page not Found");
+//     }
+//
+//
+// });
+var server =http.createServer((req,res)=>{
     console.log(req.headers);
-
-    if(urlParsed.pathname=='/echo' && urlParsed.query.message){
-        res.end(`it is ok. Message is ${urlParsed.query.message}`);
-    }else{
-        res.statusCode = 404;
-        res.end("Page not Found");
-    }
-
-
-});
+    var urlParsed = url.parse(req.url, true);
+        if(urlParsed.pathname=='/echo' && urlParsed.query.message){
+            // res.writeHead(200, 'OK',{'Cach-control':'no-cache'});
+            res.setHeader('Cach-control', 'no-cache');
+            res.end(`it is ok. Message is ${urlParsed.query.message}`);
+        }else{
+            res.statusCode = 404;
+            res.end("Page not Found");
+        }
+})
 server.listen(1337,'127.0.0.1');
