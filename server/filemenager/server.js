48b66a547dd8778d8bac9591eb00e05cd4d4114a
Pavel <akulov_pavel88@mail.ru>
2017-09-06 18:54:09 +0300

    read file

--- server/filemenager/server.js
+++ server/filemenager/server.js
@@ -24,8 +24,18 @@ const server = http.createServer((req, res) => {
 
 }).listen(3000);
 
-function read_file(res) {
-    res.end('read_file')
+function read_file(req,res) {
+    var length = 0;
+    req.on('data', function(chunk) {
+      // ничего не делаем с приходящими данными, просто считываем
+      length += chunk.length;
+      if (length > 50 * 1024 * 1024) {
+        res.statusCode = 413;
+        res.end("File too big");
+      }
+    }).on('end', function() {
+      res.end('read_file');
+    });
 }
 function save_file(res) {
     res.end('save_file')
